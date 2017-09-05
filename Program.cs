c060647db23accc7b5f7119d9f3556dffe4504d1
Jacob Leatherwood <jacob.leatherwood.22@gmail.com>
2017-09-05 12:38:20 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConditionalExcersises
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
